80d1ee2e29a48c14762fb4527f3ffb8b5249e9b7
LucasEdwa <lucas.eduardo2070@gmail.com>
2024-10-15 16:31:42 +0200

    Search function, fetch, localStorage to last search results.

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,8 @@
 import "./style.css";
+import IOmbdbResponse from "./models/IOmbdResponse";
+import IMovie from "./models/IMovie";
+//#region localstorage
+
 localStorage.setItem("name", "Lucas");
 
 const myName = localStorage.getItem("name");
@@ -9,10 +13,52 @@ const meAndMyself = [{ name: "sebastian", age: 45, isMarried: true }];
 
 localStorage.setItem("theList", JSON.stringify(meAndMyself));
 
-const myself = localStorage.getItem('theList')
-if(myself){
-    const personList:[] = JSON.parse(myself);
-    for(let i=0; i<personList.length;i++){
-        console.log(personList[i])
-    }
+const myself = localStorage.getItem("theList");
+if (myself) {
+  const personList: [] = JSON.parse(myself);
+  for (let i = 0; i < personList.length; i++) {
+    console.log(personList[i]);
+  }
 }
+//#endregion
+
+const createHTML = (movies: IMovie[]) => {
+  let moviesBox = document.getElementById("movies");
+  if (moviesBox) {
+    moviesBox.innerHTML = "";
+  }
+
+  for (let i = 0; i < movies.length; i++) {
+    let movie = document.createElement("div");
+    movie.id = "movie";
+
+    let title = document.createElement("h2");
+    let poster = document.createElement("img");
+
+    title.innerHTML = movies[i].Title;
+    poster.src = movies[i].Poster;
+    movie.appendChild(title);
+    movie.appendChild(poster);
+    moviesBox?.appendChild(movie);
+  }
+};
+
+document.getElementById("searchForm")?.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let textInput = document.getElementById("textInput") as HTMLInputElement;
+  let input = textInput.value;
+
+  fetch("http://www.omdbapi.com/?i=tt3896198&apikey=1efdfa84&s=" + input)
+    .then((response) => response.json())
+    .then((result: IOmbdbResponse) => {
+      localStorage.setItem("movies", JSON.stringify(result.Search));
+      createHTML(result.Search);
+
+      textInput.value = "";
+    });
+});
+
+//store last search
+const movies: IMovie[] = JSON.parse(localStorage.getItem("movies") || "[]");
+
+createHTML(movies);
---
+++ src/models/IMovie.ts
@@ -0,0 +1,5 @@
+export default interface IMovie{
+    Title: string,
+    ImbdID: string,
+    Poster: string,
+}
